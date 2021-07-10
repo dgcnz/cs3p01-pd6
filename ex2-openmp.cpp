c08e84cf4c4151f03a09b3c83210282d5d67d99d
Diego Canez <canezdiego@gmail.com>
2021-07-09 22:27:03 -0500

    done ex2

--- ex2-openmp.cpp
+++ ex2-openmp.cpp
@@ -1,46 +1,82 @@
 #include <iostream>
 #include <omp.h>
 
-void calculo_a(double*, int) {}
-void calculo_b(double*, int) {}
-void calculo_c(double*, int) {}
+void calculo_a(double *, int) {}
+void calculo_b(double *, int) {}
+void calculo_c(double *, int) {}
 
-double OpVec(double A[], double B[], double C[], int n);
+double OpVec(double A[], double B[], double C[], int n)
+{
+    double res;
+#pragma omp parallel
+#pragma omp single
+    {
+        double s1, s2;
 
-int main() {
-	int n = 10;
-	double a[n], b[n], c[n];
+#pragma omp task default(none) shared(A) firstprivate(n) depend(out : A)
+        calculo_a(A, n); /* funcion calculo a */
+#pragma omp task default(none) shared(B) firstprivate(n) depend(out : B)
+        calculo_b(B, n); /* funcion calculo b */
+#pragma omp task default(none) shared(C) firstprivate(n) depend(out : C)
+        calculo_c(C, n); /* funcion calculo c */
 
-	OpVec(a, b, c, n);
+#pragma omp task default(none) shared(A, B, s1) firstprivate(n) depend(in      \
+                                                                       : A, B) \
+    depend(out                                                                 \
+           : A, s1)
+        {
+            for (int i = 0; i < n; i++)
+            { /* primer bucle for*/
+                s1 = 0;
+                for (int j = 0; j < n; j++)
+                    s1 += A[i] * B[i];
+                for (int j = 0; j < n; j++)
+                    A[i] *= s1;
+            }
+        }
 
-	return 0;
+#pragma omp task default(none) shared(B, C, s2) firstprivate(n) depend(in      \
+                                                                       : B, C) \
+    depend(out                                                                 \
+           : C, s2)
+        {
+            for (int i = 0; i < n; i++)
+            { /* segundo bucle for */
+                s2 = 0;
+                for (int j = 0; j < n; j++)
+                    s2 += B[i] * C[i];
+                for (int j = 0; j < n; j++)
+                    C[i] *= s2;
+            }
+        }
+#pragma omp task default(none) shared(res, C) firstprivate(s1, s2, n)          \
+    depend(in                                                                  \
+           : s1, s2, C) depend(out                                             \
+                               : res)
+        {
+            /* calculo final */
+            double a = s1 / s2;
+            res      = 0;
+
+            for (int i = 0; i < n; i++)
+                res += a * C[i];
+        }
+#pragma omp taskwait
+    }
+    return res;
 }
 
-double OpVec(double A[], double B[], double C[], int n) {
-	int i, j;
-	double s1, s2, a, res;
+int main()
+{
+    int    n = 2;
+    double a[n], b[n], c[n];
+    a[0] = 1, a[1] = 2;
+    b[0] = 1, b[1] = 2;
+    c[0] = 1, c[1] = 2;
 
-	calculo_a(A, n); /* funcion calculo a */
-	calculo_b(B, n); /* funcion calculo b */
-	calculo_c(C, n); /* funcion calculo c */
+    double res = OpVec(a, b, c, n);
 
-	for (i = 0; i < n; i++) { /* primer bucle for*/
-		s1 = 0;
-		for (j = 0; j < n; j++) s1 += A[i] * B[i];
-		for (j = 0; j < n; j++) A[i] *= s1;
-	}
+    printf("%f\n", res);
 
-	for (i = 0; i < n; i++) { /* segundo bucle for */
-		s2 = 0;
-		for (j = 0; j < n; j++) s2 += B[i] * C[i];
-		for (j = 0; j < n; j++) C[i] *= s2;
-	}
-
-	/* calculo final */
-	a = s1 / s2;
-	res = 0;
-
-	for (i = 0; i < n; i++) res += a * C[i];
-
-	return res;
+    return 0;
 }
