d69554b9963bfc7a9a99e2f610b86aa8014621f3
Maria <mlovatonv@gmail.com>
2021-07-09 22:53:55 -0500

    some fixes

--- ex3/measure_mpi.py
+++ ex3/measure_mpi.py
@@ -2,25 +2,39 @@ import matplotlib.pyplot as plt
 from subprocess import check_output
 
 TESTS = 100
-
+P_TESTS = [2, 4, 8, 16]
 COMMAND = "mpirun -np {0} ./ex3.out"
 
-tests = []
-for p in range(1, 16):
-	print("Process {}".format(p))
-	total = 0.0
 
-	for i in range(TESTS):
-		out = check_output(COMMAND.format(p).split()).decode("utf-8")
-		total += float(out.split()[1])
-	
-	total /= TESTS
+def measure_time(p):
+    total = 0.0
 
-	tests.append(total)
+    for i in range(TESTS):
+        out = check_output(COMMAND.format(p).split()).decode("utf-8")
+        total += float(out.split()[1])
 
-plt.figure(figsize=(10, 8))
-plt.title("Execution time vs. number of processes")
-plt.ylabel("Execution time (ms)")
-plt.xlabel("Number of processes")
-plt.plot([i for i in range(1, 16)], tests)
-plt.savefig("ex3.png")
+    total /= TESTS
+
+    return total
+
+
+def main():
+    single_p_time = measure_time(1)  # single processor
+
+    tests = []
+
+    for p in P_TESTS:
+        print("Process {}".format(p))
+        test = measure_time(p) / single_p_time  # speedup
+        tests.append(test)
+
+    plt.figure(figsize=(10, 8))
+    plt.title("Speedup vs. number of processes")
+    plt.ylabel("Speedup")
+    plt.xlabel("Number of processes")
+    plt.plot(P_TESTS, tests)
+    plt.savefig("ex3.png")
+
+
+if __name__ == "__main__":
+    main()
